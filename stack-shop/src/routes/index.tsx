4a4630805ed282330a4f1c01f9626b7b600d8390
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-01-20 14:35:39 +0530

    improve card structure in App component

--- stack-shop/src/routes/index.tsx
+++ stack-shop/src/routes/index.tsx
@@ -8,26 +8,36 @@ function App() {
   return (
     <div
       className="space-y-12 px-2 py-2 md:px-4 md:py-8 lg:px-8 lg:py-16
-    items-center justify-center flex flex-col"
+     flex flex-col
+    bg-linear-to-b from-slate-50 via-white to-slate-50
+    p-6"
     >
-      <Card className="inline-flex space-y-0.5 md:max-w-2xl px-4">
+      <Card
+        className="space-y-1 md:max-w-6xl
+       pl-4 pr-6"
+      >
         <p className="text-blue-700 font-bold text-nm">
           YOUR FAVOURITE E-COMMERCE STORE
         </p>
+        <div>
+          <h1 className="text-4xl font-bold max-w-6xl">
+            Welcome to StartShop,
+          </h1>
+          <p className="text-4xl font-bold max-w-6xl">
+            Your one-stop shop for all your needs
+          </p>
+        </div>
 
-        <h1 className="text-4xl font-bold ">Welcome to StartShop</h1>
-        <p className="text-4xl font-bold ">
-          Your one-stop shop for all your needs
-        </p>
-
-        <div className="px-4 py-2">
+        <Link to="/products" className="px-4 py-2">
           <button
             className="inline-flex items-center gap-2
-            bg-black text-white px-4 py-2 rounded-full font-semibold"
+            bg-black text-white px-4 py-2 rounded-full
+             font-semibold shadow-lg transition hover:-translate-y-0.5
+             hover:shadow-xl cursor-pointer"
           >
             Browse products <ArrowRight size={16} />
           </button>
-        </div>
+        </Link>
       </Card>
     </div>
   )
