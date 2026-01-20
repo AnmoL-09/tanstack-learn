714675a88476e045faca34d6f24f6b9e8a6a0b03
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-01-20 15:33:43 +0530

    add fakestoreapi for products page

--- stack-shop/src/routes/index.tsx
+++ stack-shop/src/routes/index.tsx
@@ -2,9 +2,20 @@ import { Card } from '@/components/ui/card'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { ArrowRight } from 'lucide-react'
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({
+  component: App,
+  loader: async () => {
+    // This runs on server during SSR AND on client during navigation
+    const response = await fetch('https://fakestoreapi.com/products')
+    const data = await response.json()
+    console.log('Fetched products data:', data.slice(0, 4))
+    return { products: data }
+  },
+})
 
 function App() {
+  const { products } = Route.useLoaderData()
+  console.log(products)
   return (
     <div
       className="space-y-12 px-2 py-2 md:px-4 md:py-8 lg:px-8 lg:py-16
@@ -12,33 +23,35 @@ function App() {
     bg-linear-to-b from-slate-50 via-white to-slate-50
     p-6"
     >
-      <Card
-        className="space-y-1 md:max-w-6xl
-       pl-4 pr-6"
-      >
-        <p className="text-blue-700 font-bold text-nm">
-          YOUR FAVOURITE E-COMMERCE STORE
-        </p>
-        <div>
-          <h1 className="text-4xl font-bold max-w-6xl">
-            Welcome to StartShop,
-          </h1>
-          <p className="text-4xl font-bold max-w-6xl">
-            Your one-stop shop for all your needs
+      <section>
+        <Card
+          className="space-y-1 md:max-w-6xl
+        pl-4 pr-6"
+        >
+          <p className="text-blue-700 font-bold text-nm">
+            YOUR FAVOURITE E-COMMERCE STORE
           </p>
-        </div>
+          <div>
+            <h1 className="text-4xl font-bold max-w-6xl">
+              Welcome to StartShop,
+            </h1>
+            <p className="text-4xl font-bold max-w-6xl">
+              Your one-stop shop for all your needs
+            </p>
+          </div>
 
-        <Link to="/products" className="px-4 py-2">
-          <button
-            className="inline-flex items-center gap-2
-            bg-black text-white px-4 py-2 rounded-full
-             font-semibold shadow-lg transition hover:-translate-y-0.5
-             hover:shadow-xl cursor-pointer"
-          >
-            Browse products <ArrowRight size={16} />
-          </button>
-        </Link>
-      </Card>
+          <Link to="/products" className="px-4 py-2">
+            <button
+              className="inline-flex items-center gap-2
+              bg-black text-white px-4 py-2 rounded-full
+              font-semibold shadow-lg transition hover:-translate-y-0.5
+              hover:shadow-xl cursor-pointer"
+            >
+              Browse products <ArrowRight size={16} />
+            </button>
+          </Link>
+        </Card>
+      </section>
     </div>
   )
 }
